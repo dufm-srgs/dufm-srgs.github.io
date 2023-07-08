c6d9465315977f708b31ea1879c30ca1feb16635
Tristan <LeoDog896@hotmail.com>
2023-07-08 13:35:57 -0400

    feat: base section descriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,20 @@
 
 <h2>Analyze Problems</h2>
 
+<p>
+    We explore various topics in mathematics, challenging ourselves to think critically and creatively about the world around us.
+    Whether working alone or in groups, this course prepares you to tackle a wide variety of problems with confidence.
+</p>
+
 <h2>Explore and Create</h2>
 
+<p>
+    Explore problems via graphs, simulations, and other visualizations. Look for patterns and connections in your data
+    and use them to conjecture and prove new results.
+</p>
+
 <h2>Prove and Justify</h2>
+
+<p>
+    Back up your conjectures with rigorous proofs. Learn to write proofs that are clear, concise, and convincing.
+</p>
