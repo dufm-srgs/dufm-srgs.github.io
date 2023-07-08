de510aeb9e2685941ab03f5754c69b066d9c9dba
Tristan <LeoDog896@hotmail.com>
2023-07-08 13:38:18 -0400

    feat: main wrapping, prerendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,35 @@
-<h1>Mathematics Unleashed</h1>
+<main>
+    <div class="hero">
+        <h1>Mathematics Unleashed</h1>
 
-<p>Explore mathematics beyound the boundaries of high school in the Dirt Under the Fingernails Mathematics course @ Virginia Summer Residential Governers School.</p>
+        <p>Explore mathematics beyound the boundaries of high school in the Dirt Under the Fingernails Mathematics course @ Virginia Summer Residential Governers School.</p>
+    </div>
 
-<h2>Analyze Problems</h2>
+    <h2>What we do</h2>
 
-<p>
-    We explore various topics in mathematics, challenging ourselves to think critically and creatively about the world around us.
-    Whether working alone or in groups, this course prepares you to tackle a wide variety of problems with confidence.
-</p>
+    <h3>Analyze Problems</h3>
 
-<h2>Explore and Create</h2>
+    <p>
+        We explore various topics in mathematics, challenging ourselves to think critically and creatively about the world around us.
+        Whether working alone or in groups, this course prepares you to tackle a wide variety of problems with confidence.
+    </p>
 
-<p>
-    Explore problems via graphs, simulations, and other visualizations. Look for patterns and connections in your data
-    and use them to conjecture and prove new results.
-</p>
+    <h3>Explore and Create</h3>
 
-<h2>Prove and Justify</h2>
+    <p>
+        Explore problems via graphs, simulations, and other visualizations. Look for patterns and connections in your data
+        and use them to conjecture and prove new results.
+    </p>
 
-<p>
-    Back up your conjectures with rigorous proofs. Learn to write proofs that are clear, concise, and convincing.
-</p>
+    <h3>Prove and Justify</h3>
+
+    <p>
+        Back up your conjectures with rigorous proofs. Learn to write proofs that are clear, concise, and convincing.
+    </p>
+</main>
+
+<style>
+    main {
+        padding: 1rem;
+    }
+</style>
