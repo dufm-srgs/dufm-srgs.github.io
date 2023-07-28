2cf1c5b0a88ee1d6961d606dabd26aac83fb0153
Tristan F <leodog896@gmail.com>
2023-07-27 20:59:58 -0400

    fix: seconds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -171,7 +171,7 @@
 		flex-direction: row;
 		flex-wrap: nowrap;
 		height: 10rem;
-		animation: slideshow 5s linear infinite;
+		animation: slideshow 20s linear infinite;
 		width: 200%;
 		
 		img {
